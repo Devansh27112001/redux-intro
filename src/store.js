3e390539073bb0178de7927e2bc0cd59c60e8dda
Devansh27112001 <dkansara2711@gmail.com>
2024-06-04 16:54:19 -0400

    Creating a store using redux and subscribing to that store using react-redux

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
-function store() {
-  return <div></div>;
-}
+import { combineReducers, createStore } from "redux";
+import accountReducer from "./features/account/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
+const store = createStore(rootReducer);
 
 export default store;
